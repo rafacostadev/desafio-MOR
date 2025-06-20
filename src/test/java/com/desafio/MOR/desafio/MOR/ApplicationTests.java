58d010eee6d459f3b10399bf1e795234b6eaeaaf
rafacostadev <rafaelcostamantis@gmail.com>
2025-06-20 00:22:25 -0300

    inicial

---
+++ src/test/java/com/desafio/MOR/desafio/MOR/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.MOR.desafio.MOR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
